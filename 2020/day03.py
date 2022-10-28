c9f347e2a50e04dac3d64a04d253bc50cf801154
Mark McBride <markmcb@gmail.com>
2022-10-28 17:30:49 -0400

    2020 Day 3

--- 2020/day03.py
+++ 2020/day03.py
@@ -12,30 +12,50 @@ max_col = 0
 lines = []
 file = open('input03.txt', 'r')
 for line in file:
-  max_row += 1
   line = line.strip()
-  for i in range(2):
-    line += line
-
-  if len(line) > max_col:
-    max_col = len(line)
   lines.append(line)
 
-orig = np.zeros((max_row, max_col), dtype='U1')
-for row in range(len(lines)):
-  line = lines[row]
-  for col in range(len(line)):
-    orig[row][col] = line[col]
+col = len(lines[0])
+row = len(lines)
 
-row = 0
+
+while col < row * 7:
+  lines = [line + line for line in lines]
+  col = len(lines[0])
+
 col = 0
-count = 0
-while row < max_row:
-  if orig[row][col] == '#':
-    count += 1
+total = 0
+count_a = 0
+count_b = 0
+count_c = 0
+count_d = 0
+count_e = 0
 
-  row += 1
-  col += 3
+for row in range(1, len(lines)):
+  # a is R1 D1
+  if lines[row][row] == '#':
+    count_a += 1
 
-stdout.print()
-print(count)
+  # b is Col 3 row 1
+  if lines[row][row*3] == '#':
+    count_b += 1
+
+  # c is row 1 col 5
+  if lines[row][row*5] == '#':
+    count_c += 1
+
+  # d is row 1 col 7
+  if lines[row][row*7] == '#':
+    count_d += 1
+
+  # e is row 2 col 1
+  if row * 2 > len(lines):
+    continue
+
+  if lines[row*2][row] == '#':
+    count_e += 1
+
+total = count_a  * count_b * count_c * count_d * count_e
+print(count_a, count_b, count_c, count_d, count_e)
+print('Part 1 {}'.format(count_b))
+print('Part 2 {}'.format(total))
